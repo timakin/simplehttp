3abd1e4c7f79bb5fa81dcdc53c2c5c91ec8563dc
timakin <timaki.st@gmail.com>
2015-09-17 23:40:24 +0900

    [fix]

--- server.rb
+++ server.rb
@@ -1,19 +1,52 @@
 require 'socket'
 
-port = 4000
-server = TCPServer.open(port)
 
-loop { 
-	socket = server.accept
+RESOURCE_ROOT = './public'
+HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE']
 
-	p socket.peeraddr
 
-	while buffer = socket.gets
-		p buffer
-		socket.puts 200
-	end
+class HTTPServer
+  def initialize
+    @path = '/index.html'
+    @port = 4000
+    @server = TCPServer.open(@port)
+  end
 
-	socket.close
-}
+  def requested_path(buffer)
+    @path = (buffer =~ /^GET/) ? RESOURCE_ROOT + '/index.html' : @path
+  end
 
-server.close
+  def path_init
+    @path = '/index.html'
+  end
+
+  def response_header(bytesize)
+    "HTTP/1.1 200 OK\r\n" +
+    "Content-Type: text/plain\r\n" +
+    "Content-Length: #{bytesize}\r\n"
+  end
+
+  def run
+    loop do
+      socket = @server.accept   
+
+      begin
+        while buffer = socket.gets
+          p buffer
+          @path = requested_path(buffer)
+          content = @path
+          socket.puts response_header(content.bytesize)
+          socket.puts "\r\n"
+          socket.puts content
+        end
+        path_init
+      rescue
+        socket.close
+      end
+    end
+    @server.close
+  end
+end
+
+http = HTTPServer.new
+http.run
